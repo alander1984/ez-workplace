1afe7f047d15a7a93060755d6d114db9ccce27c3
alander <alander@tzoom.pro>
2021-08-04 21:05:19 +0400

    Init commit

---
+++ src/main/java/ru/egartech/workplace/WorkplaceApplication.java
@@ -0,0 +1,13 @@
+package ru.egartech.workplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/egartech/workplace/WorkplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.egartech.workplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
